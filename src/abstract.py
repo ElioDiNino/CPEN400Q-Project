abce657c42678165b6fa80a5f6585c7badca732d
Richard Sun <richardsun.gz@gmail.com>
2025-03-22 15:32:19 -0700

    feat(data): added load data function + added self param to other class methods

--- src/abstract.py
+++ src/abstract.py
@@ -1,28 +1,43 @@
 from numpy import ndarray
 from abc import ABC, abstractmethod
-
+import csv
+import numpy as np
 
 class ForecastingMethod(ABC):
     def __init__():
         """Basic initialization stuff"""
         pass
 
-    def load_data(filepath: str) -> dict[str, ndarray]:
-        pass
+    def load_data(self, filepath: str, windows_size: int = 12) -> dict[str, ndarray]:
+        with open(filepath, mode='r') as file:
+            reader = csv.reader(file)
+            data = list(reader)[1:] # don't include the header
 
-    def preprocess_data(X: ndarray, y: ndarray) -> dict[str, ndarray]:
+        # convert data to float
+        data = [float(datapoint[0]) for datapoint in data]
+        X = np.zeros((len(data) - windows_size, windows_size))
+        y = np.zeros(len(data) - windows_size)
+        
+        # stop window right before the last value so it can be used in y
+        for i in range(len(data) - windows_size):
+            X[i] = (data[i:i+windows_size])
+            y[i] = (data[i+windows_size])
+        
+        return {'X': X, 'y': y}
+
+    def preprocess_data(self, X: ndarray, y: ndarray) -> dict[str, ndarray]:
         """Applies differencing and normalization to the input data"""
         pass
 
     @abstractmethod
     def train(
-        train_X: ndarray, train_y: ndarray, val_X: ndarray, val_y: ndarray
+        self, train_X: ndarray, train_y: ndarray, val_X: ndarray, val_y: ndarray
     ) -> None:
         """Run model training with training and validation data"""
         pass
 
     @abstractmethod
-    def save_weights(filepath: str) -> bool:
+    def save_weights(self, filepath: str) -> bool:
         """
         Save the computed weights from training. Returns an error if training
         has not yet run or no weights have been loaded
@@ -30,11 +45,31 @@ class ForecastingMethod(ABC):
         pass
 
     @abstractmethod
-    def load_weights(filepath: str) -> bool:
+    def load_weights(self, filepath: str) -> bool:
         """Load saved weights from a file or data structure"""
         pass
 
     @abstractmethod
-    def predict(X: ndarray) -> ndarray:
+    def predict(self, X: ndarray) -> ndarray:
         """Predict future values based on input data"""
         pass
+
+class Test(ForecastingMethod):
+    def __init__(self):
+        pass
+
+    def train(
+        train_X: ndarray, train_y: ndarray, val_X: ndarray, val_y: ndarray
+    ) -> None:
+        pass
+
+    def save_weights(filepath: str) -> bool:
+        pass
+
+    def load_weights(filepath: str) -> bool:
+        pass
+
+    def predict(X: ndarray) -> ndarray:
+        pass
+obj = Test()
+print(obj.load_data('./data/paper-data.csv', 2))
