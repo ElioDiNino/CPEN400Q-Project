1c2b2604cd1ca6db92110ddbb91cde3eac6ffd34
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2025-04-05 15:35:14 -0700

    feat(train): timing reporting for each method

--- src/train.py
+++ src/train.py
@@ -1,9 +1,19 @@
 from linear_regression import train as lr_train
 from neural_network import train as nn_train
 from pqc import train as pqc_train
+import time
 
 if __name__ == "__main__":
-    lr_train()
-    nn_train()
-    pqc_train()
+    training_functions = [
+        ("Linear Regression", lr_train),
+        ("Neural Network", nn_train),
+        ("PQC", pqc_train),
+    ]
+
+    for name, func in training_functions:
+        start_time = time.time()
+        func()
+        execution_time = time.time() - start_time
+        print(f"{name} training completed in {execution_time:.2f} seconds.")
+
     print("\nTraining complete.")
